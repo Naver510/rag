2aa19d0bcb55a470cf4f1c5bb48b60100a0bb720
Michał Kumięga <101349466+Naver510@users.noreply.github.com>
2025-10-20 23:24:37 +0200

    Com2_2.2

--- lab2/script.js
+++ lab2/script.js
@@ -40,6 +40,7 @@
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(posts => {
+        console.log(posts)
         const parts = ['<div style="display:flex;flex-wrap:wrap;gap:12px">']
         posts.slice(0, 12).forEach(p => {
           parts.push(
